ae83ccd4528f8c4a887c5be2574a5f05a2e2e654
kboston91 <kourtneygboston@gmail.com>
2022-01-04 17:48:07 -0600

    corrected category delete route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -95,7 +95,7 @@ Category.destroy({
     id: req.params.id
   }
 })
-.then(dbCategoryData => {
+.then((dbCategoryData) => {
   if (!dbCategoryData) {
     res.status(404).json({ message: 'There was no category found with this id.' });
     return;
